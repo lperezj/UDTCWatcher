fb26d5cc7d571944347e4d6d17d8d2348d7d5965
Luis Perez Jimenez <luis.perez.jimenez@avanade.com>
2018-12-03 12:04:41 +0100

    Add project files.

---
+++ UDTCWatcher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+
+namespace UDTCWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ACPIX Watcher!");
+            Program p = new Program();
+            p.Start();
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private void Start()
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = ConfigurationManager.AppSettings["FolderWatch"];
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | 
+                                   NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            FileInfo acspixFile = new FileInfo(e.FullPath);
+            Console.WriteLine($"New ACSPIX {acspixFile.FullName}");
+            // TODO DeCryptSinovoFile
+            // TODO ProcessAcspix
+            // TODO Remove File
+        }
+    }
+}
